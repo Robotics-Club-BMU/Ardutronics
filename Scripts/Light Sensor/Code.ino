1eb0f1ac2db65961c87e9f0ac34e85e0f752b965
Anmol Jhamb <codingwithaj@gmail.com>
2021-10-22 18:30:28 +0530

    made the pin numbers to be constant

--- Scripts/Light Sensor/Code.ino
+++ Scripts/Light Sensor/Code.ino
@@ -1,10 +1,11 @@
+#define SENSOR 9
 int sensorValue = 0;
 
 void setup()
 {
     pinMode(A0, INPUT);
     Serial.begin(9600);
-    pinMode(9, OUTPUT);
+    pinMode(SENSOR, OUTPUT);
 }
 
 void loop()
@@ -14,6 +15,6 @@ void loop()
     // print the sensor reading so you know its range
     Serial.println(sensorValue);
     // map the sensor reading to a range for the LED
-    analogWrite(9, map(sensorValue, 0, 1023, 0, 255));
+    analogWrite(SENSOR, map(sensorValue, 0, 1023, 0, 255));
     delay(100); // Wait for 100 millisecond(s)
 }
